2f581d132d7169a0737444f968441201fc103311
Afferreira03 <alan.fferreira03@gmail.com>
2024-06-11 17:18:46 -0300

    chore: Adição de variável de ambiente

--- src/main/java/com/singular/blogapijava/BlogApiJavaApplication.java
+++ src/main/java/com/singular/blogapijava/BlogApiJavaApplication.java
@@ -2,8 +2,10 @@ package com.singular.blogapijava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class BlogApiJavaApplication {
 
 	public static void main(String[] args) {
